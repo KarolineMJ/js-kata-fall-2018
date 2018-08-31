13adf549ed1c87cbf599f71df55bf46a97b7282b
KarolineMJ <karoline_mj@hotmail.com>
2018-08-31 13:43:57 +0200

    cookies working

--- w1d2 - this site uses cookies/mysolution/script.js
+++ w1d2 - this site uses cookies/mysolution/script.js
@@ -1,7 +1,25 @@
-let boxAnimation = document.querySelector(".cookieBox");
+"use strict"
 
-window.onload = function () {
-    setInterval(function){
-        boxAnimation, 3000
+//let boxAnimation = document.querySelector(".cookieBox");
+
+window.addEventListener("load", function findBTN() {
+    const cookieBox = document.querySelector(".cookieBox");
+    const acceptBTN = document.querySelector(".acceptBTN");
+    cookieBox.classList.add("slideIn");
+
+    removeCookies();
+    function removeCookies() {
+        acceptBTN.addEventListener("click", function removeThis() {
+            cookieBox.classList.remove("slideIn");
+        });
     }
+
 }
+);
+
+
+//let acceptance = localStorage.setItem();
+
+
+//localStorage.setItem("accepted", "VeryAccepted")  - accepted
+//if(localStorage )
