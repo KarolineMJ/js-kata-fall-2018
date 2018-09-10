9bef423a3885db248b2f48f4aa84f1fd7e9532fb
KarolineMJ <karoline_mj@hotmail.com>
2018-09-10 12:53:24 +0200

    background-transition done

---
+++ w4d2 - background-transition/mysolution/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + r + "," + g + "," + b + ")";
+
+    document.querySelector("div").style.background = bgColor;
+}
+
+setInterval(randomColor, 3000);
